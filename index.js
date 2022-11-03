a7d26a31cb22ea1acc497301d57b42dd383c7cb5
Jakub Koza <jakub.koza13@gmail.com>
2022-11-03 20:44:43 +0100

    add mongodb

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const mongoClient = require('mongodb').MongoClient
 
 const app = express()
 
-app.get('/', (req,res) => {
-  console.log('hello')
+const uri = 'mongodb+srv://flashitclient:FlashItClient1@flashit.vb8uf3z.mongodb.net/?retryWrites=true&w=majority'
+
+mongoClient.connect(uri, {}, (error, client) => {
+  if (error) console.log('Cannot connect to database')
+
+  const db = client.db('flashit')
+
+  db.collection('user').insertOne({
+    username: 'jakkoz',
+    password: 'jakkoz123'
+  }, (error, result) => {
+    if (error) console.log('Cannot insert')
+
+    console.log(result)
+  })
+})
+
+app.get('/', (req, res) => {
   res.send('Hello')
 })
 
