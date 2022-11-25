3a54b000d02a07025a5ee8e86d1a23b64e4078d3
Amen't <milosz.koza12@gmail.com>
2022-11-25 23:55:34 +0100

    .get to /folder

--- routes/folder-route.js
+++ routes/folder-route.js
@@ -1,58 +1,75 @@
-const router = require('express').Router()
-const folderRepo = require('../services/repos/folder-repo')
-const auth = require("../services/auth/auth-middleware")
-var mongo = require('mongodb')
+const router = require('express').Router();
+const folderRepo = require('../services/repos/folder-repo');
+const auth = require('../services/auth/auth-middleware');
+var mongo = require('mongodb');
+const folderService = require('../application/folder');
+const {
+  sendResponseBasedOnService,
+} = require('../services/api/api-reponse-base-on-service');
 
-router.route('/')
+router
+  .route('/')
   .post(auth, async (req, res) => {
-    const { user_id } = req.user
-    const { name, from_lang, to_lang } = req.body
+    const { user_id } = req.user;
+    const { name, from_lang, to_lang } = req.body;
 
     const response = await folderRepo.create({
       name: name,
       from_lang: from_lang,
       to_lang: to_lang,
-      owner_id: user_id
-    })
+      owner_id: user_id,
+    });
 
-    console.log(response)
+    console.log(response);
 
-    res.status(201).send()
+    res.status(201).send();
   })
+  .get(auth, async (req, res) => {
+    const { user_id } = req.user;
+    return sendResponseBasedOnService(
+      res,
+      await folderService.getFolderByUserID(user_id)
+    );
+  });
 
-router.route('/contribution')
-  .post(auth, async (req, res) => {
-    const { user_id } = req.user
-    const { folder_id, contributor_id, permission_type } = req.body
+router.route('/contribution').post(auth, async (req, res) => {
+  const { user_id } = req.user;
+  const { folder_id, contributor_id, permission_type } = req.body;
 
-    const findResult = await folderRepo.find({
-      _id: new mongo.ObjectID(folder_id),
-      owner_id: user_id
-    })
+  const findResult = await folderRepo.find({
+    _id: new mongo.ObjectID(folder_id),
+    owner_id: user_id,
+  });
 
-    if (!findResult.isSuccess) return res.status(404).send('Cannot find folder.')
+  if (!findResult.isSuccess) return res.status(404).send('Cannot find folder.');
 
-    var folder = findResult.data
+  var folder = findResult.data;
 
-    if (folder.owner_id === contributor_id) return res.status(400).send('You cannot be a contributor of your own folder.')
-    
-    if (folder.contributors === undefined) folder.contributors = []
+  if (folder.owner_id === contributor_id)
+    return res
+      .status(400)
+      .send('You cannot be a contributor of your own folder.');
 
+  if (folder.contributors === undefined) folder.contributors = [];
 
-    if (folder.contributors.find(c => c.contributor_id = contributor_id) != null) return res.status(400).send('Already added contributor.')
+  if (
+    folder.contributors.find(c => (c.contributor_id = contributor_id)) != null
+  )
+    return res.status(400).send('Already added contributor.');
 
-    folder.contributors.push({
-      contributor_id: contributor_id,
-      permission_type: permission_type
-    })
+  folder.contributors.push({
+    contributor_id: contributor_id,
+    permission_type: permission_type,
+  });
 
-    console.log(folder)
+  console.log(folder);
 
-    const updateResult = await folderRepo.update(folder)
+  const updateResult = await folderRepo.update(folder);
 
-    if (!updateResult.isSuccess) return res.status(400).send('Cannot update folder.')
+  if (!updateResult.isSuccess)
+    return res.status(400).send('Cannot update folder.');
 
-    res.status(201).send()
-  })
+  res.status(201).send();
+});
 
-module.exports = router
+module.exports = router;
--- services/repos/folder-repo.js
+++ services/repos/folder-repo.js
@@ -1,47 +1,50 @@
-const dbProvider = require('../db/mongo-connection')
+const dbProvider = require('../db/mongo-connection');
 
-const create = async (folder) => {
-  const db = await dbProvider()
-  var result = await db.collection('folder').insertOne(folder)
+const create = async folder => {
+  const db = await dbProvider();
+  var result = await db.collection('folder').insertOne(folder);
   return {
     isSuccess: true,
     error: '',
-    data: null
-  }
-}
+    data: null,
+  };
+};
 
-const find = async (filter) => {
-  const db = await dbProvider()
-  var result = await db.collection('folder').findOne(filter)
-
-  if (result === null) return {
-    isSuccess: false,
-    error: 'Cannot find folder.',
-    data: null
-  }
-
-  return {
-    isSuccess: true,
-    error: '',
-    data: result
-  }
-}
-
-const update = async (folder) => {
-  const db = await dbProvider()
-  var result = await db.collection('folder').replaceOne({ _id: folder._id }, folder, { upsert: true } )
-
-  if (result === null) return {
-    isSuccess: false,
-    error: 'Cannot update folder.',
-    data: null
-  }
+const find = async filter => {
+  const db = await dbProvider();
+  const result = await db.collection('folder').find(filter).toArray();
+  if (result === null)
+    return {
+      isSuccess: false,
+      error: 'Cannot find folder.',
+      data: null,
+    };
 
   return {
     isSuccess: true,
     error: '',
-    data: result
-  }
-}
+    data: result,
+  };
+};
 
-module.exports = { create, find, update }
+const update = async folder => {
+  const db = await dbProvider();
+  var result = await db
+    .collection('folder')
+    .replaceOne({ _id: folder._id }, folder, { upsert: true });
+
+  if (result === null)
+    return {
+      isSuccess: false,
+      error: 'Cannot update folder.',
+      data: null,
+    };
+
+  return {
+    isSuccess: true,
+    error: '',
+    data: result,
+  };
+};
+
+module.exports = { create, find, update };
